0a358a34cf3932832fe77bf626d59f3d05fb4c89
Cecilia Knäbchen <uqrnh@student.kit.edu>
2025-05-03 22:50:01 +0200

    Minor changes
    - Added a line to README
    - Changed file ending from .thir to .th
    - test.th is now run by thirscript.test.Test instead of
    thirscript.Thirscript

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Thirscript
-The Thirscript Programming Language
+The Thirscript Programming Language.
+
+A dynamically-typed concatenative prototype-based language.
---
+++ src/thirscript/test/Test.java
@@ -0,0 +1,54 @@
+package thirscript.test;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+import thirscript.JavaFunction;
+import thirscript.ThInteger;
+import thirscript.ThObject;
+import thirscript.Var;
+import thirscript.expr.Expr;
+import thirscript.parse.Lexer;
+import thirscript.parse.Parser;
+
+public class Test
+{
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, IOException
+    {
+        // Lexer lexer = new Lexer(new InputStreamReader(System.in));
+        Lexer lexer = new Lexer(Files.newBufferedReader(Paths.get("test.th")));
+        Parser parser = new Parser();
+        
+        Expr n = parser.parse(lexer.iterator());
+        System.out.println(n);
+        
+        Map<String, Var> start = new HashMap<>();
+
+        // put builtin "types"
+        start.put("Object", Var.constant(ThObject.OBJECT));
+
+        // put builtin values
+        start.put("null", ThObject.NULL);
+        start.put("false", Var.constant(ThInteger.FALSE));
+        start.put("true", Var.constant(ThInteger.TRUE));
+
+        // put print
+        {
+            MethodType print_t = MethodType.methodType(void.class, Object.class);
+            MethodHandle print = MethodHandles.lookup().findVirtual(PrintStream.class, "println", print_t);
+            print = MethodHandles.insertArguments(print, 0, System.out);
+            print = print.asType(print_t.changeParameterType(0, ThObject.class));
+            print = MethodHandles.filterReturnValue(print, MethodHandles.constant(ThObject.class, ThInteger.TRUE));
+            start.put("print", Var.constant(new JavaFunction(print)));
+        }
+
+        System.out.println(n.eval(start));
+    }
+}
