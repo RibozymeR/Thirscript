cab0abfaca4137c0c4eec82b5edd2dc25b30626f
Cecilia Knäbchen <uqrnh@student.kit.edu>
2025-05-20 23:14:45 +0200

    Renamed Token.value to the more accurate Token.content

--- src/thirscript/parse/Token.java
+++ src/thirscript/parse/Token.java
@@ -2,14 +2,14 @@ package thirscript.parse;
 
 import java.util.Set;
 
-public record Token(TokenType type, String value, int line, int column)
+public record Token(TokenType type, String content, int line, int column)
 {
 
 	public static final Token EOF = new Token(TokenType.EOF, -1, -1);
 
 	public Token
 	{
-		if(type == TokenType.IDENTIFIER && TokenType.reserved.contains(value))
+		if(type == TokenType.IDENTIFIER && TokenType.reserved.contains(content))
 			throw new IllegalArgumentException("Identifier token at " + line + ":" + column + " should be keyword");
 	}
 
@@ -20,8 +20,8 @@ public record Token(TokenType type, String value, int line, int column)
 
 	public String toString()
 	{
-		if(value != null)
-			return type.toString() + "[" + value + "]";
+		if(content != null)
+			return type.toString() + "[" + content + "]";
 		else
 			return type.toString();
 	}
@@ -42,7 +42,8 @@ public record Token(TokenType type, String value, int line, int column)
 		ASSIGN, ASSIGNC, /* = := */
 		IF, ELSE, WHILE, NEW, /* keywords */
 		OP, /* + - * / % & | ^ ~ == < <= > >= != */
-		LPAREN, RPAREN, LBRACE, RBRACE, EOF; /* ( ) { } */
+		LPAREN, RPAREN, LBRACE, RBRACE, /* ( ) { } */
+		EOF; /* signals end of file/input */
 
 		public static final Set<String> reserved = Set.of("if", "else", "while", "new");
 	}
